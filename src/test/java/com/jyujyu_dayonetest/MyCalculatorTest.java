ff7d77df99e7e211b2760f3591cf01bd618a1224
koosco <rnxogud123@naver.com>
2024-03-06 12:10:37 +0900

    계산기 테스트 코드 작성

---
+++ src/test/java/com/jyujyu_dayonetest/MyCalculatorTest.java
@@ -0,0 +1,68 @@
+package com.jyujyu_dayonetest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class MyCalculatorTest {
+
+    @Test
+    public void addTest() throws Exception {
+        // given
+        MyCalculator myCalculator = new MyCalculator();
+
+        // when
+        myCalculator.add(10.0);
+
+        // then
+        assertEquals(10.0, myCalculator.getResult());
+    }
+
+    @Test
+    public void minusTest() throws Exception {
+        // given
+        MyCalculator myCalculator = new MyCalculator(10.0);
+
+        // when
+        myCalculator.minus(5.0);
+
+        // then
+        assertEquals(5.0, myCalculator.getResult());
+    }
+
+    @Test
+    public void multiplyTest() throws Exception {
+        // given
+        MyCalculator myCalculator = new MyCalculator(2.0);
+
+        // when
+        myCalculator.multiply(2.0);
+
+        // then
+        assertEquals(4.0, myCalculator.getResult());
+    }
+
+    @Test
+    public void divideTest() throws Exception {
+        // given
+        MyCalculator myCalculator = new MyCalculator(10.0);
+
+        // when
+        myCalculator.divide(2.0);
+
+        // then
+        assertEquals(5.0, myCalculator.getResult());
+    }
+    
+    @Test
+    public void divideZero() throws Exception {
+        // given
+        MyCalculator myCalculator = new MyCalculator(10.0);
+
+        // when & then
+        assertThrows(MyCalculator.ZeroDivisionException.class, () -> {
+            myCalculator.divide(0.0);
+        });
+    }
+}
